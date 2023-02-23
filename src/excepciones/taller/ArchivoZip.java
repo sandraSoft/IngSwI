3a59c4410ecf513f2ccc9e241ad7e09476000519
Sandra Hurtado <sandra.hurtado@ucaldas.edu.co>
2023-02-23 11:16:16 -0500

    Ajustes en los comentarios

--- src/excepciones/taller/ArchivoZip.java
+++ src/excepciones/taller/ArchivoZip.java
@@ -1,2 +1,22 @@
-package excepciones.taller;public class ArchivoZip {
+package excepciones.taller;
+
+import java.io.IOException;
+import java.util.zip.ZipFile;
+
+/**
+ * Clase para operaciones con un archivo zip
+ *
+ * @version  1.0
+ */
+public class ArchivoZip {
+
+    /**
+     * Obtener la cantidad de archivos en un zip,
+     * a partir del nombre completo del archivo.
+     */
+    public int cantidadArchivos(String rutaArchivo)
+            throws IOException {
+        ZipFile archivoZip = new ZipFile(rutaArchivo);
+        return archivoZip.size();
+    }
 }
--- src/excepciones/taller/ProgramaZip.java
+++ src/excepciones/taller/ProgramaZip.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ProgramaZip {
+package excepciones.taller;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ProgramaZip {
+    public static void main(String[] args) {
+        ProgramaZip programa = new ProgramaZip();
+        programa.mostrarCantidadArchivos();
+    }
+
+    /**
+     * Pide el nombre de un archivo zip y muestra la cantidd
+     *  de archivos que tiene internamente
+     */
+    public void mostrarCantidadArchivos() {
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Ruta archivo: ");
+        String rutaArchivo = consola.next();
+
+        ArchivoZip archivo = new ArchivoZip();
+        try {
+            int cantidadArchivos = archivo.cantidadArchivos(rutaArchivo);
+            System.out.println("Cantidad: "+cantidadArchivos);
+        } catch (IOException errorArchivo){
+            System.out.println("La ruta no corresponde a un" +
+                    "archivo zip. Revisar.");
+        }
+    }
 }
