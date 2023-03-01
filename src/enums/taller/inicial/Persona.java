b17832e94c80405e021c799b0a75a1583633e460
Sandra Hurtado <sandra.hurtado@ucaldas.edu.co>
2023-02-28 19:19:22 -0500

    Versión inicial punto 2 taller enumeraciones

---
+++ src/enums/taller/inicial/Persona.java
@@ -0,0 +1,39 @@
+package enums.taller.inicial;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ * Información de una persona que será usada en una notaría.
+ *
+ * Ejercicio para el uso de ENUM.
+ */
+public class Persona {
+    private String nombre;
+    private String estadoCivil;
+    private LocalDate fechaNacimiento;
+
+    public Persona(String nombre, String estadoCivil, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.estadoCivil = estadoCivil;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public int getEdad(){
+        LocalDate fechaHoy = LocalDate.now();
+        Period periodo = Period.between(fechaNacimiento, fechaHoy);
+        return periodo.getYears();
+    }
+}
